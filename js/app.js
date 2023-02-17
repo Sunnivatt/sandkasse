a7fa4ca7004d961a1669b932298c1e45cc2eed31
Sunniva Tømmerbakk <sunnivatryggeset@gmail.com>
2023-02-17 22:36:05 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+
+function list() {
+    const list = document.querySelector('ul');
+    const input = document.querySelector('input');
+    const button = document.querySelector('button');
+
+    const inputElement = document.getElementById('fill');
+
+    const buttonElement = document.getElementById('add');
+
+    const itemListElement = document.getElementById('item-list');
+
+
+    buttonElement.addEventListener('click', renderList);
+
+    window.addEventListener('keyup', (event) => {
+        if(event.code === 'Enter') {
+            renderList();
+        }
+    }) ;
+
+    function renderList(){
+        if(inputElement.value !== '') {
+            const liElement = document.createElement('li');
+            const itemButton = document.createElement('button');
+            itemButton.textContent = 'fjern';
+            itemButton.id = 'deleteButton';
+            liElement.textContent = inputElement.value;
+            liElement.appendChild(itemButton);
+            itemListElement.appendChild(liElement);
+            inputElement.value = '';
+            itemButton.addEventListener('click', () => {
+            itemListElement.removeChild(liElement);
+            })
+        } else {
+            alert('skriv inn gyldig verdi')
+        }
+    }
+}
+list();
